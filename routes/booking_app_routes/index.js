83df69f6f13acb1fae57470b1b8eccd8b6c1ff68
PayDevD <mrpaydfordev@gmail.com>
2019-11-16 18:04:59 +0900

    fetch workplace api -find workplaces by category api -find workplaces by workplaceID api

--- routes/booking_app_routes/index.js
+++ routes/booking_app_routes/index.js
@@ -1,26 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
-/* get Category Data */
-router.get('/', (req, res, next) => {
-  res.json({
-    name : "Dummny",
-    color: '#1abc9c'
-  })
-});
-
-
-router.get('/second', (req, res, next) => {
-    res.json({
-      name : 'Second',
-      color: '#1abc9c'
-    })
-});
-
-
-// store page api
+var WorkPlace = require('../../schemas/workplace');
 
 
 // store selector api
+router.get('/stores/:category', (req, res, next) => {
+  WorkPlace.find({"Category": req.params.category}, (err, workplaces) => {
+    if(err) return res.status(500).send({error: 'database failure'});
+    res.json(workplaces);
+  })
+});
+// store page api
+router.get('/store/:store_id', (req, res, next) => {
+  WorkPlace.find({"WorkPlaceID" : req.params.store_id}, (err, workplaces) => {
+    if(err) return res.status(500).send({error: 'database failure'});
+    res.json(workplaces);
+  })
+});
 
 module.exports = router;
