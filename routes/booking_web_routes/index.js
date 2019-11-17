f164b7c1c68f5430b26fb671ddacf45b8bab0736
PayDevD <mrpaydfordev@gmail.com>
2019-11-17 18:55:18 +0900

    WorkPlace API -create, modify, get workplace -get reservation related with workplace -get account data api

--- routes/booking_web_routes/index.js
+++ routes/booking_web_routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const shortHash = require("short-hash");
 const SelfEmployed = require("../../schemas/self_employed");
+const WorkPlace = require("../../schemas/workplace");
+const Reservation = require("../../schemas/reservation_transaction");
 
 // SelfEmployed ID duplication check
 router.get("/selfemployed/:id", (req, res, next) => {
@@ -69,6 +72,94 @@ router.put("/account/:id", (req, res, next) => {
     console.log(output);
     if(!output.n) return res.status(404).json({error: "Account not found"});
     res.json({ messgae: "Account Updated"});
+  })
+})
+
+// account data check
+router.get("/account/data/:id", (req, res, next) => {
+  SelfEmployed.findOne({ ID: req.params.id }, (err, account) => {
+    if(err) res.status(500).json({error: "database failure"});
+    if(account === null) {
+      res.json({
+        result: 0
+      })
+    }
+    else {
+      res.json({
+        Email: account.Email,
+        PhoneNumber: account.PhoneNumber
+      });
+    }
+  })
+})
+
+
+// workplace register
+router.post("/workplace", (req, res, next) => {
+  let workplace = new WorkPlace();
+  workplace.SelfEmployedID = req.body.SelfEmployedID;
+  workplace.WorkPlaceID = shortHash(req.body.Address);
+  workplace.Address = req.body.Address;
+  workplace.WorkPlaceInfo = req.body.WorkPlaceInfo;
+  workplace.Name = req.body.Name;
+  workplace.Category = req.body.Category;
+  workplace.save((err) => {
+    if(err) {
+      console.log(err);
+      res.json({result: 0});
+      return;
+    }
+
+    res.json({
+      result: 1
+    });
+  })
+})
+
+// workplace lookup
+router.get("/workplace/:workplaceID", (req, res, next) => {
+  WorkPlace.findOne({ WorkPlaceID: req.params.workplaceID }, (err, workplace) => {
+    if(err) res.status(500).json({error: "database failure"});
+
+    if(workplace === null) {
+      res.json({
+        result: 0
+      })
+    }
+    else {
+      res.json({
+        Address: workplace.Address,
+        WorkPlaceInfo: workplace.WorkPlaceInfo,
+        Name: workplace.Name,
+        Category: workplace.Category
+      });
+    }
+  })
+})
+
+// workplace reservation find
+router.get("/workplace/reservation/:workplaceID", (req, res, next) => {
+  Reservation.find({ WorkPlaceID: req.params.workplaceID }, (err, reservations) => {
+    if(err) res.status(500).json({error: "database failure"});
+
+    if(reservations.length === 0) {
+      res.json({
+        result: 0
+      })
+    }
+    else {
+      res.json(reservations);
+    }
+  })
+})
+
+// workplace modify
+router.put("/workplace/update/:workplaceID", (req, res, next) => {
+  WorkPlace.update({ WorkPlaceID: req.params.workplaceID }, { $set: req.body }, (err, output) => {
+    if(err) res.status(500).json({error: "database failure"});
+    console.log(output);
+    if(!output.n) return res.status(404).json({error: "WorkPlace not found"});
+    res.json({ messgae: "WorkPlace Updated"});
   })
 })
 
